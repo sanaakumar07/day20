507d501d65cd0f41247805cd9cc497ba6b8ca947
sanaa <Sanaakumar07@gmail.com>
2026-02-21 13:49:48 +0530

    c

--- day20/main.c
+++ day20/main.c
@@ -1,15 +1,31 @@
-//
-//  main.c
-//  day20
-//
-//  Created by Sanaa Kumar on 21/02/26.
-//
-
-#include <stdlib.h>
 #include <stdio.h>
+int main(void)
+{
+int n, i, j;
+int a[100];
+int count = 0, sum;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
-    return EXIT_SUCCESS;
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter elements:\n");
+    for(i = 0; i < n; i++)
+    {
+        scanf("%d", &a[i]);
+    }
+
+    for(i = 0; i < n; i++)
+    {
+        sum = 0;
+        for(j = i; j < n; j++)
+        {
+            sum += a[j];
+            if(sum == 0)
+                count++;
+        }
+    }
+
+    printf("Number of subarrays with sum 0 = %d\n", count);
+
+    return 0;
 }
